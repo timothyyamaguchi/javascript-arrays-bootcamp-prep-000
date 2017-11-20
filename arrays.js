bb5d0cd49121fc75198f1ace74e66e84e1385bd5
Tim Yamaguchi <timothy.yamaguchi@comcast.net>
2017-11-20 22:37:59 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,5 +6,11 @@ var chocolateBars = [
 ]
 
 function addElementToBeginningOfArray(array, element) {
+  var chocolateBars = [
+    "snickers",
+    "hundred grand",
+    "kit kat",
+    "skittles"
+  ]
   ["baby ruth", ...chocolateBars]
 }
